ad0c83cbe5dc48a11341aba0546f8f32b7bc0518
NadiaPoi <nadia.poi.fou@gmail.com>
2019-09-30 15:51:23 +0200

    first commit

---
+++ src/reducers/areArticlesBeingFetched.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+const areArticlesBeingFetched = (state = false, action) => {
+  switch (action.type) {
+    case 'FETCH_ARTICLES__BEGIN':
+      return true;
+    case 'FETCH_ARTICLES__RESOLVE':
+      return false;
+    default:
+      return state;
+  }
+};
+
+export default areArticlesBeingFetched;
